d323759f3a4c651f6dc96dafe8a86a9a54efb8f0
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2023-02-04 11:37:09 -0700

    header component built

---
+++ alexareact/src/Title.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+    return (
+        <>
+        <div className="Title">
+            <h1>SFPOPOS</h1>
+        </div><div className="Title-Subtitle">
+                <h2>San Franciscos Privately Owned Public Spaces</h2>
+            </div>
+        </>
+    )
+}
+
+export default Title
