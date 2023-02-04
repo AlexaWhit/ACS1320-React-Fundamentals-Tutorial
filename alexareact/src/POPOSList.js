ea032226cf410d901c8a396eb3bcb863c7c0fbb9
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2023-02-04 15:07:38 -0700

    content component built

---
+++ alexareact/src/POPOSList.js
@@ -0,0 +1,40 @@
+import POPOSSpace from "./POPOSSpace"
+
+function POPOSList() {
+    return (
+        <div className="POPOSList">
+            <POPOSSpace
+                name="50 California Street"
+                address="50 California St."
+                image="50-california-st.jpg" 
+            />
+            <POPOSSpace
+                name="100 Pine Street"
+                address="100 Pine St."
+                image="100-pine.jpg" 
+            />
+            <POPOSSpace
+                name="101 California Street"
+                address="101 California St."
+                image="101-california.jpg" 
+            />
+            <POPOSSpace
+                name="Transamerica Redwood Park"
+                address="600 Montgomery St."
+                image="transamerica-redwood-park.jpg" 
+            />
+            <POPOSSpace
+                name="343 Sansome Roof Garden"
+                address="343 Sansome St. Floor 15"
+                image="343-sansome-roof-garden.jpg" 
+            />
+            <POPOSSpace
+                name="525 Market Street Plaza"
+                address="525 Market St."
+                image="525-market-street-plaza.jpg" 
+            />
+        </div>
+    )
+}
+
+export default POPOSList
--- alexareact/src/Title.js
+++ alexareact/src/Title.js
@@ -1,4 +1,6 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
+import logo from './logo.svg';
 import './Title.css';
 
 function Title() {
@@ -8,6 +10,7 @@ function Title() {
             <h1>SFPOPOS</h1>
         </div><div className="Title-Subtitle">
                 <h2>San Franciscos Privately Owned Public Spaces</h2>
+                <img src={logo} />
             </div>
         </>
     )
