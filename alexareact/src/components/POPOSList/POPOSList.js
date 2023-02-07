666bb92e0b6242980a4e6eb2e7fa0b204974eb30
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2023-02-07 12:57:24 -0700

    stretch completed - added search functionality

--- alexareact/src/components/POPOSList/POPOSList.js
+++ alexareact/src/components/POPOSList/POPOSList.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 import POPOSSpace from '../POPOSSpace/POPOSSpace';
 import './POPOSList.css';
-import data from '../../sfpopos-data.json';
+import data from '../../sfpopos-data.js';
 
 function POPOSList() {
-	const spaces = data.map(( { title, address, images, hours }, i) => {
+	const [query, setQuery] = useState('')
+	const spaces = data.filter((obj) => {
+		const inTitle = obj.title.toLowerCase().includes(query.toLowerCase())
+		const inAddress = obj.address.toLowerCase().includes(query.toLowerCase())
+		return inTitle || inAddress
+	}).map((obj) => {
+		const { id, title, address, images, hours } = obj
+	// const spaces = data.map(({ title, address, images, hours }, i) => {
 		return (
 			<div className="POPOSList">
-			<POPOSSpace
-				id={i}
-				key={title}
-				name={title}
-				address={address}
-				image={images[0]}
-				hours={hours}
-			/>
+				<POPOSSpace
+					id={id}
+					key={`${title}-${id}`}
+					name={title}
+					address={address}
+					image={images[0]}
+					hours={hours}
+				/>
 			</div>
 		)
 	})
 
 	return (
 		<div className="POPOSList">
-			{ spaces }
+			<form>
+				<input
+					value={query}
+					placeholder="search"
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+			{spaces}
 		</div>
 	)
 }
