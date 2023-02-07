0fef6337c58edcbaf9c0c052798f845f10ffe081
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2023-02-07 10:54:28 -0700

    stretch completed - organized files

--- alexareact/src/components/App.js
+++ alexareact/src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import Title from './Title';
-import Footer from './Footer';
+import Title from './Title/Title';
+import Footer from './Footer/Footer';
 
 
 function App() {
